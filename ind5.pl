1149a0f758309437468d20b10249d2b37c42404c
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-12 00:28:26 +0300

    Update ind5.pl
    Добавлена генерация всех перестановок

--- ind5.pl
+++ ind5.pl
@@ -1,4 +1,5 @@
 :- dynamic sochet/1.
+:- dynamic words/1.
 
 read_str(A,N):-get0(X),r_str(X,A,[],N,0).
 r_str(10,A,A,N,N):-!.
@@ -25,7 +26,7 @@ delete_char([H|T],El,B,C):-append(C,[H],C1),delete_char(T,El,B,C1).
 
 delete_repeat(A,B):-delete_repeat(A,B,[]).
 delete_repeat([],B,B):-!.
-delete_repeat([H|T],B,C):-append(C,[H],C1),delete_char([H|T],H,NewList,[]),delete_repeat(NewList,B,C1).
+delete_repeat([H|T],B,C):-append(C,[H],C1),delete_char([H|T],H,NewList),delete_repeat(NewList,B,C1).
 
 add_module_two(A,B):-C=[a,b,c,d,e,f],add_module_two(C,A,B,[]).
 add_module_two([],_,D,D):-!.
@@ -36,16 +37,24 @@ list_without_twice_char(A,C):-delete_repeat(A,B),add_module_two(B,C).
 
 
 %Выводит все сочетания;
-comb:-read(A),write("Введите длину: "),read(N),
-		(N>=8,N=<10->
-		      comb(A,N,B),(not(sochet(B)),repeat_twice(B,B)->
-				  asserta(sochet(B)),list_without_twice_char(B,C),
-				   append(B,C,D),write(D),nl,fail);write("Неверная длина"),fail).
+comb:-A=[a,a,b,b,c,c,d,d,e,e,f,f],
+		write("Введите длину слов:\n"),read(N),
+		(N>=8,N=<10->comb(A,N,B),(not(sochet(B)),repeat_twice(B,B)->
+					 asserta(sochet(B)),list_without_twice_char(B,C),append(B,C,D),all_perestanovki(D),fail)
+		;write("Неверная длина"),fail).
 comb([],0,[]).
 comb([X|List],N, [X|Tail]) :-N1 is N-1,comb(List,N1, Tail).
 comb([_|List],N, Tail) :-comb(List,N, Tail).
 
+write_str([]):-!.
+write_str([H|T]):-put(H),write_str(T).
 
+all_perestanovki(A):-perestanovka(A,P),(not(words(P))->asserta(words(P)),write(P),nl,fail).
 
+perestanovka([],[]).
+perestanovka(List,[H|Perm]):-delete(H,List,Rest),perestanovka(Rest,Perm).
+
+delete(X,[X|T],T).
+delete(X,[H|T],[H|NT]):-delete(X,T,NT).
 
 
