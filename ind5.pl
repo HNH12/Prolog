99ff2990bc72eb49b047802d36168a83a703ef77
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-11 23:39:46 +0300

    Create ind5.pl
    Формирование списка для генерации слов

---
+++ ind5.pl
@@ -0,0 +1,51 @@
+:- dynamic sochet/1.
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+pr1:-read(X),name(A,X),asserta(sochet(A)).
+
+
+member([X|_],X):-!.
+member([_|L],X):-member(L,X),!.
+
+count_repeat(A,El,N):-count_repeat(A,El,N,0).
+count_repeat([],_,N,N):-!.
+count_repeat([H|T],El,N,K):-H=El,K1 is K+1,count_repeat(T,El,N,K1),!.
+count_repeat([_|T],El,N,K):-count_repeat(T,El,N,K).
+
+repeat_twice([],_):-!.
+repeat_twice([H|T],A):-count_repeat(A,H,N),!,N=2,repeat_twice(T,A).
+
+delete_char(A,El,B):-delete_char(A,El,B,[]).
+delete_char([],_,B,B):-!.
+delete_char([H|T],H,B,C):-delete_char(T,H,B,C),!.
+delete_char([H|T],El,B,C):-append(C,[H],C1),delete_char(T,El,B,C1).
+
+delete_repeat(A,B):-delete_repeat(A,B,[]).
+delete_repeat([],B,B):-!.
+delete_repeat([H|T],B,C):-append(C,[H],C1),delete_char([H|T],H,NewList,[]),delete_repeat(NewList,B,C1).
+
+add_module_two(A,B):-C=[a,b,c,d,e,f],add_module_two(C,A,B,[]).
+add_module_two([],_,D,D):-!.
+add_module_two([H|T],B,D,C):-member(B,H),add_module_two(T,B,D,C),!.
+add_module_two([H|T],B,D,C):-append(C,[H],C1),add_module_two(T,B,D,C1).
+
+list_without_twice_char(A,C):-delete_repeat(A,B),add_module_two(B,C).
+
+
+%Выводит все сочетания;
+comb:-read(A),write("Введите длину: "),read(N),
+		(N>=8,N=<10->
+		      comb(A,N,B),(not(sochet(B)),repeat_twice(B,B)->
+				  asserta(sochet(B)),list_without_twice_char(B,C),
+				   append(B,C,D),write(D),nl,fail);write("Неверная длина"),fail).
+comb([],0,[]).
+comb([X|List],N, [X|Tail]) :-N1 is N-1,comb(List,N1, Tail).
+comb([_|List],N, Tail) :-comb(List,N, Tail).
+
+
+
+
+
