1ac2bc75a06093ce025d31cdc0c3af3c95837c82
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-13 07:27:56 +0300

    Update ind5.pl
    Генерация слов
    7 вариант

--- ind5.pl
+++ ind5.pl
@@ -8,8 +8,8 @@ r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 pr1:-read(X),name(A,X),asserta(sochet(A)).
 
 
-member([X|_],X):-!.
-member([_|L],X):-member(L,X),!.
+member1([X|_],X):-!.
+member1([_|L],X):-member1(L,X),!.
 
 count_repeat(A,El,N):-count_repeat(A,El,N,0).
 count_repeat([],_,N,N):-!.
@@ -28,20 +28,23 @@ delete_repeat(A,B):-delete_repeat(A,B,[]).
 delete_repeat([],B,B):-!.
 delete_repeat([H|T],B,C):-append(C,[H],C1),delete_char([H|T],H,NewList),delete_repeat(NewList,B,C1).
 
-add_module_two(A,B):-C=[a,b,c,d,e,f],add_module_two(C,A,B,[]).
+add_module_two(A,B):-C=[97,98,99,100,101,102],add_module_two(C,A,B,[]).
 add_module_two([],_,D,D):-!.
-add_module_two([H|T],B,D,C):-member(B,H),add_module_two(T,B,D,C),!.
+add_module_two([H|T],B,D,C):-member1(B,H),add_module_two(T,B,D,C),!.
 add_module_two([H|T],B,D,C):-append(C,[H],C1),add_module_two(T,B,D,C1).
 
-list_without_twice_char(A,C):-delete_repeat(A,B),add_module_two(B,C).
+list_without_twice_char(A,8):-all_perestanovki(A).
+list_without_twice_char(A,9):-delete_repeat(A,B),add_module_two(B,C),perestanovka(C,[H|_]),append(A,[H],D),all_perestanovki(D).
+list_without_twice_char(A,10):-delete_repeat(A,B),add_module_two(B,C),append(A,C,D),all_perestanovki(D).
 
 
 %Выводит все сочетания;
-comb:-A=[a,a,b,b,c,c,d,d,e,e,f,f],
+comb:-A=[97,97,98,98,99,99,100,100,101,101,102,102],
 		write("Введите длину слов:\n"),read(N),
-		(N>=8,N=<10->comb(A,N,B),(not(sochet(B)),repeat_twice(B,B)->
-					 asserta(sochet(B)),list_without_twice_char(B,C),append(B,C,D),all_perestanovki(D),fail)
+		(N>=8,N=<10->comb(A,8,B),(not(sochet(B)),repeat_twice(B,B)->
+					 tell('Word1.txt'),asserta(sochet(B)),list_without_twice_char(B,N),told,fail)
 		;write("Неверная длина"),fail).
+
 comb([],0,[]).
 comb([X|List],N, [X|Tail]) :-N1 is N-1,comb(List,N1, Tail).
 comb([_|List],N, Tail) :-comb(List,N, Tail).
@@ -49,12 +52,23 @@ comb([_|List],N, Tail) :-comb(List,N, Tail).
 write_str([]):-!.
 write_str([H|T]):-put(H),write_str(T).
 
-all_perestanovki(A):-perestanovka(A,P),(not(words(P))->asserta(words(P)),write(P),nl,fail).
+all_perestanovki(A):-perestanovka(A,P),(not(words(P))->asserta(words(P)),write_str(P),nl,fail).
 
 perestanovka([],[]).
-perestanovka(List,[H|Perm]):-delete(H,List,Rest),perestanovka(Rest,Perm).
+perestanovka(List,[H|Perm]):-delete1(H,List,Rest),perestanovka(Rest,Perm).
 
-delete(X,[X|T],T).
-delete(X,[H|T],[H|NT]):-delete(X,T,NT).
+delete1(X,[X|T],T).
+delete1(X,[H|T],[H|NT]):-delete1(X,T,NT).
+
+
+
+
+
+
+
+
+
+
+
 
 
