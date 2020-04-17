1d96cbbd222d57767881b82e144109716d4cff3f
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-17 20:31:52 +0300

    Create 1_5.fs

---
+++ 1_5.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let delit a b =
+    if (a%b=0) then false
+    else true
+
+let rec prost a b =
+    if a = b then true
+    else (delit a b) && (prost a (b+1))
+
+let pr a =
+    match a with
+    |1-> false
+    |_-> prost a 2
+
+
+let pr1_5 = 
+    System.Console.WriteLine("Введите число: ")
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    (pr a)
+
+[<EntryPoint>]
+let main argv =
+
+    let Answer = pr1_5
+    System.Console.WriteLine(Answer)
+
+    0 // return an integer exit code
