519a524248f5fedeb925415fb5688a0c72431462
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-17 20:31:50 +0300

    Create 1_4.fs

---
+++ 1_4.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let rec fib n =
+    match n with
+    |1->1
+    |2->1
+    |_->fib(n-1)+fib(n-2)
+
+let pr1_4 =
+    System.Console.WriteLine("Введите число: ")
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    (fib a)
+
+[<EntryPoint>]
+let main argv =
+
+    let Answer = pr1_4
+    System.Console.WriteLine(Answer)
+
+    0 // return an integer exit code
