e92ee654f6683769efc1b2ca904e2ba7f345d1b7
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-17 20:51:27 +0300

    Create 1_7.fs

---
+++ 1_7.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let rec mult a =
+    if (a/10 = 0) then a
+    else a%10 * mult(a/10)
+
+let rec sum a =
+    match a with
+    |0->0
+    |_->a%10 + sum (a/10)
+
+let funct f a = f a
+
+let pr1_7 =
+    System.Console.WriteLine("Введите число: ")
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    (funct mult a, funct sum a)
+
+[<EntryPoint>]
+let main argv = 
+
+    let answer = pr1_7
+    System.Console.WriteLine(answer)
+
+    0 // return an integer exit code
