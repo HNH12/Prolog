624054bc4f7c39c1ec9e6e637bb370128db04bb0
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-04-17 20:29:30 +0300

    Create 1_3.fs

---
+++ 1_3.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let rec fact n =
+    match n with
+    | 1->1
+    | _->n*fact(n-1)
+
+let pr1_3 =
+    System.Console.WriteLine("Введите число: ")
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    (fact a)
+
+[<EntryPoint>]
+let main argv =
+
+    let Answer = pr1_3
+    System.Console.WriteLine(Answer)
+
+    0 // return an integer exit code
