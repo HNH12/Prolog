7c5ff1f32dcaf047559ec1c1d289fc3f6a90648d
HNH12 <62258989+HNH12@users.noreply.github.com>
2020-05-11 10:12:39 +0300

    Update

--- HomeWork(With_List)/HomeWork(With_List)/Program.fs
+++ HomeWork(With_List)/HomeWork(With_List)/Program.fs
@@ -58,12 +58,81 @@ let rec write_list list =
 //let prog list =
 //    List.filter (fun elem -> (check1 elem) && (check2 list elem 0 % 2 = 0)) list
 
+// 13.4
+
+//let rec sum_digit elem sum =
+//    match elem with
+//    |0->sum
+//    |_ -> sum_digit (elem/10) (sum+elem%10)
+
+//let prog list =
+//    List.filter (fun elem -> (elem > 0) || (sum_digit (abs elem) 0 > 10)) list
+
+// 13.5
+
+//let rec sq_elem list elem =
+//    if (List.isEmpty(list)) then false
+//    else if (elem = list.Head * list.Head) then true
+//         else sq_elem list.Tail elem
+
+//let prog list =
+//    (List.filter (fun elem -> sq_elem list elem) list).Length
+
+// 13.7
+
+let rec sum_digit elem sum =
+    match elem with
+    |0->sum
+    |_ -> sum_digit (elem/10) (sum+elem%10)
+
+let rec count_delit el delit count =
+   match delit=el with
+   |true -> count+1
+   |false -> if (el%delit = 0) then count_delit el (delit+1) (count+1)
+             else count_delit el (delit+1) count
+
+let compare_f elem1 elem2 =
+    if (elem1 < elem2)  then 1
+    else -1
+
+let compare_s elem1 elem2 =
+    if (sum_digit (abs elem1) 0 > sum_digit (abs elem2) 0) then 1
+    else -1
+
+let compare_t elem1 elem2 =
+    if (count_delit elem1 1 0 < count_delit elem2 1 0) then 1
+    else -1
+
+let rec form_cortezh f_list s_list t_list new_list i index = 
+    
+let max a b =
+    if (a>b) then a
+    else b
+
+let prog f_list s_list t_list n =
+    let sorted_f_list = List.sortWith compare_f f_list
+    let sorted_s_list = List.sortWith compare_s s_list
+    let sorted_t_list = List.sortWith compare_t t_list
+    form_cortezh sorted_f_list sorted_s_list sorted_t_list new_list 0 n
+    
 
 
 [<EntryPoint>]
 let main argv =
-    let list = read_data
-    let ans = prog list
-    Console.WriteLine(ans.ToString())
+    printf "Введите длину первого списка: "
+    let n1 = Convert.ToInt32(System.Console.ReadLine());
+    printf "Введите первый список: \n"
+    let f_list = read_list n1
+    printf "Введите длину второго списка: "
+    let n2 = Convert.ToInt32(System.Console.ReadLine());
+    printf "Введите второй список: \n"
+    let s_list = read_list n2
+    printf "Введите длину третьего списка: "
+    let n3 = Convert.ToInt32(System.Console.ReadLine());
+    printf "Введите третий список: \n"
+    let t_list = read_list n3
+    printf "\nИтоговый список:\n"
+    let ans = prog f_list s_list t_list (max (max n1 n2) n3)
+    write_list ans
     0 // return an integer exit code
 
